f140261be49dbd9e1bf3afdd3b289de6ea5521d1
Sundar <sundars@krazybee.com>
2023-02-08 19:18:42 +0530

    Finished Gatsby Tutorial Part 3

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,35 +1,53 @@
-import * as React from 'react'
-import { Link } from 'gatsby'
-import { 
+import * as React from "react";
+import { Link, useStaticQuery, graphql } from "gatsby";
+import {
   container,
+  siteTitle,
   heading,
   navLinks,
   navLinkItem,
-  navLinkText } from './layout.module.css'
+  navLinkText,
+} from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
   return (
     <div className={container}>
-    <nav>
-      <ul className={navLinks}>
-        <li className={navLinkItem}>
-          <Link to="/" className={navLinkText}>
-            Home
-          </Link>
-        </li>
-        <li className={navLinkItem}>
-          <Link to="/about" className={navLinkText}>
-            About
-          </Link>
-        </li>
-      </ul>
-    </nav>
-    <main>
-      <h1 className={heading}>{pageTitle}</h1>
-      {children}
-    </main>
-  </div>
-  )
-}
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+      <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>
+              Blog
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
